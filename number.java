fed45c9624709e462ac43223cacc49bbbc9b4277
ahanadas <das.ahana095@gmail.com>
2017-07-09 14:55:39 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+         {
+           int a;
+           System.out.println("Enter the number:");
+           if(a>0)
+           System.out.println("Positive no");
+           else if(a<0)
+           System.out.println("Negative no");
+           else if(a=0)
+           System.out.println("Zero");
+         }
+}
